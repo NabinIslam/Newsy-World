f08fb7617c3af2d1bdfac5e1cb4d1f25a717bc82
Nabin Islam <sinabin62246@gmail.com>
2022-07-15 00:37:45 +0600

    responsive nav little bit done

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const NavbarComponent = () => {
   let links = [
@@ -7,18 +8,47 @@ const NavbarComponent = () => {
     { name: "Sports", link: "/sports" },
     { name: "Entertainment", link: "/entertainment" },
   ];
-  let [open, setOpen] = useState(false);
+
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-    <nav className="shadow">
-      <h1>NewsyWorld</h1>
-      <ul>
-        {links.map((Link) => (
-          <li>
-            <a href={Link.link}>{Link.name}</a>
-          </li>
-        ))}
-      </ul>
+    <nav className="shadow text-white bg-black ">
+      <div className="container mx-auto flex justify-between items-center p-4 ">
+        <h1 className="font-bold text-2xl">NewsyWorld</h1>
+        <ul className="hidden md:flex">
+          {links.map((Link) => (
+            <li className="px-2">
+              <a className="font-bold" href={Link.link}>
+                {Link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div onClick={handleNav} className="cursor-pointer block md:hidden">
+          {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+        <div
+          className={
+            !nav
+              ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-500 h-full bg-black ease-in-out duration-500"
+              : "fixed left-[-100%] "
+          }
+        >
+          <ul>
+            <ul className=" px-5 pt-2">
+              {links.map((Link) => (
+                <li className="py-2 border-b border-b-gray-600">
+                  <a href={Link.link}>{Link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
